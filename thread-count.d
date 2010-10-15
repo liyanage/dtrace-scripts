3e9e9b3f135dd10243e128c8db5123c034d799ba
Marc Liyanage <liyanage@apple.com>
2010-10-14 23:04:47 -0700

    Whitespace fix.

--- thread-count.d
+++ thread-count.d
@@ -7,12 +7,12 @@ proc:::lwp-start
 /pid == $target/
 {
     this->mIndex = ++globalIndex;
-	printf("[%d] thread start, count = %d", this->mIndex, curpsinfo->pr_nlwp);
+    printf("[%d] thread start, count = %d", this->mIndex, curpsinfo->pr_nlwp);
 }
 
 proc:::lwp-exit
 /pid == $target/
 {
     this->mIndex = ++globalIndex;
-	printf("[%d] thread exit, count = %d", this->mIndex, curpsinfo->pr_nlwp);
+    printf("[%d] thread exit, count = %d", this->mIndex, curpsinfo->pr_nlwp);
 }
