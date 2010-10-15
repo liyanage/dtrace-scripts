eab3a506c43e1f8fe492578b23da81fe19bdd636
Marc Liyanage <liyanage@apple.com>
2010-10-14 23:02:04 -0700

    thread-count DTrace script.

---
+++ thread-count.d
@@ -0,0 +1,18 @@
+BEGIN
+{
+    globalIndex = 0;
+}
+
+proc:::lwp-start
+/pid == $target/
+{
+    this->mIndex = ++globalIndex;
+	printf("[%d] thread start, count = %d", this->mIndex, curpsinfo->pr_nlwp);
+}
+
+proc:::lwp-exit
+/pid == $target/
+{
+    this->mIndex = ++globalIndex;
+	printf("[%d] thread exit, count = %d", this->mIndex, curpsinfo->pr_nlwp);
+}
